bfca088e2ffab49ba90af783a23fd9b1f8d135b5
Christian Brevik <christian.brevik@gmail.com>
2018-02-22 20:54:35 +0100

    Learning about WebAssembly memory

---
+++ lesson7/program.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+struct Record
+{
+    int id;
+    float x;
+    float y;
+};
+
+struct Record *createRecord(int id, float x, float y)
+{
+    struct Record *record = malloc(sizeof(struct Record));
+    record->id = id;
+    record->x = x;
+    record->y = y;
+    return record;
+}
+
+void deleteRecord(struct Record *record)
+{
+    free(record);
+}
